cdc782470b0313c2f48a00f08b7b32d130d0cc1e
Karlsson0214 <karlsson0214@gmail.com>
2022-09-13 08:24:43 +0200

    arrow

--- Assets/Scripts/Bow.cs
+++ Assets/Scripts/Bow.cs
@@ -7,6 +7,7 @@ public class Bow : MonoBehaviour
     private Rigidbody2D rb;
     public GameObject arrowPrefab;
     private GameObject arrow;
+    private Rigidbody2D rbArrow;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,7 @@ public class Bow : MonoBehaviour
         rb.gravityScale = 0;
         rb.freezeRotation = true;
         arrow = Instantiate(arrowPrefab, new Vector3(0.14f + rb.position.x, rb.position.y, 0), Quaternion.identity);
-        Rigidbody2D rbArrow = arrow.GetComponent<Rigidbody2D>();
+        rbArrow = arrow.GetComponent<Rigidbody2D>();
         rbArrow.SetRotation(-90);
     }
 
@@ -25,11 +26,13 @@ public class Bow : MonoBehaviour
         {
             // up
             rb.SetRotation(rb.rotation + 30 * Time.deltaTime);
+            rbArrow.SetRotation(rbArrow.rotation + 30 * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             // down
             rb.SetRotation(rb.rotation - 30 * Time.deltaTime);
+            rbArrow.SetRotation((rbArrow.rotation - 30) * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.Space))
         {
